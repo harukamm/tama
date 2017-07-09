d049242686eb1b4851557d793dbbd36571154dc3
Haruka Matsumoto <harukam0416@gmail.com>
2017-07-09 13:33:01 +0900

    Change file name src/parse.ml -> src/tokenize.ml
    I was planning to implement both tokenizing and parsing in one file
    `src/parse.ml`, but I've decided to devide them into another file.

--- test/test.ml
+++ test/test.ml
@@ -1,6 +1,6 @@
 #load "util.cmo";;
 #load "types.cmo";;
-#load "parse.cmo";;
+#load "tokenize.cmo";;
 
 let () = print_endline ">>>>>>>>>>>>>>>"
 let () = print_endline " Testing..."
@@ -18,14 +18,14 @@ let () = assert ("[1; 2; 3]" = s1)
 let () = assert ("[a; b; c]" = s2)
 
 
-(* Parse test *)
-let e1 = Parse.skip (fun x -> x = '\n') (0, 0, 0, "\n\n\nAA")
-let e2 = Parse.skip (fun x -> x = 'c') (0, 0, 0, "ccc\n XX")
-let e3 = Parse.skip_spaces (0, 0, 0, "  \r \n   XX")
-let e4 = Parse.skip_num (3, 2, 3, "abc123\n  ")
+(* Tokenize test *)
+let e1 = Tokenize.skip (fun x -> x = '\n') (0, 0, 0, "\n\n\nAA")
+let e2 = Tokenize.skip (fun x -> x = 'c') (0, 0, 0, "ccc\n XX")
+let e3 = Tokenize.skip_spaces (0, 0, 0, "  \r \n   XX")
+let e4 = Tokenize.skip_num (3, 2, 3, "abc123\n  ")
 let s1 = " \n\
 \t xx "
-let e5 = Parse.skip_spaces (0, 0, 0, s1)
+let e5 = Tokenize.skip_spaces (0, 0, 0, s1)
 
 let () = assert ((3, 3, 0, "\n\n\n") = e1)
 let () = assert ((3, 0, 3, "ccc") = e2)
@@ -33,26 +33,26 @@ let () = assert ((8, 1, 3, "  \r \n   ") = e3)
 let () = assert ((6, 2, 6, "123") = e4)
 let () = assert ((4, 1, 2, " \n\t ") = e5)
 
-let e1 = Parse.skip_word (2, 0, 2, "xx44xx")
-let e2 = Parse.skip_word (3, 0, 3, "xxx-xxx")
-let e3 = Parse.skip_word (0, 0, 0, "abc123 dko")
+let e1 = Tokenize.skip_word (2, 0, 2, "xx44xx")
+let e2 = Tokenize.skip_word (3, 0, 3, "xxx-xxx")
+let e3 = Tokenize.skip_word (0, 0, 0, "abc123 dko")
 
 let () = assert ((4, 0, 4, "44") = e1)
 let () = assert ((3, 0, 3, "") = e2)
 let () = assert ((6, 0, 6, "abc123") = e3)
 
-let e1 = Parse.one_token (2, 0, 2, "12aBCxxx =")
+let e1 = Tokenize.one_token (2, 0, 2, "12aBCxxx =")
 let i1 = (0, (2, 8))
-let e2 = Parse.one_token (2, 0, 2, "12+")
+let e2 = Tokenize.one_token (2, 0, 2, "12+")
 let i2 = (0, (2, 3))
 let s1 = " \n\
 \t\txx "
-let e3 = Parse.one_token (0, 0, 0, s1)
+let e3 = Tokenize.one_token (0, 0, 0, s1)
 let i3 = (1, (2, 4))
 let s2 = "    \n\
 \t   \n\
 \t    50000 \n"
-let e4 = Parse.one_token (0, 0, 0, s2)
+let e4 = Tokenize.one_token (0, 0, 0, s2)
 let i4 = (2, (5, 10))
 
 let () = assert ((8, 0, 8, Types.VAR ("aBCxxx", i1)) = e1)
@@ -62,7 +62,7 @@ let () = assert ((20, 2, 10, Types.INT (50000, i4)) = e4)
 
 let s = "5 +  10\n\
 \t-1\n"
-let e = Parse.tokenize s
+let e = Tokenize.tokenize s
 let t1 = Types.INT (5, (0, (0, 1)))
 let t2 = Types.PLUS (0, (2, 3))
 let t3 = Types.INT (10, (0, (5, 7)))
@@ -72,7 +72,7 @@ let () = assert ([t1; t2; t3; t4; t5] = e)
 
 let s = "  xx - 1 \n\
 \t xb_x \t  *  "
-let e = Parse.tokenize s
+let e = Tokenize.tokenize s
 let t1 = Types.VAR ("xx", (0, (2, 4)))
 let t2 = Types.MINUS (0, (5, 6))
 let t3 = Types.INT (1, (0, (7, 8)))
